1b8d3531941e04c444eae2c5d7bc3274f5a9c344
rainyjonne <rainyjonne@gmail.com>
2020-12-13 01:59:46 +0800

    Copy representers (should add HATEOAS in the future)

---
+++ app/presentation/representers/restaurant_id_name_pic_representer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'roar/decorator'
+require 'roar/json'
+require_relative 'picture_representer'
+
+module Ewa
+  module Representer
+    # Represents searched restaurants' ids & names
+    class RestaurantIdNamePic < Roar::Decorator
+      include Roar::JSON
+
+      property :id
+      property :name
+      collection :pictures, extend: Representer::Picture, class: OpenStruct
+
+    end
+  end
+end
---
+++ app/presentation/representers/searched_restaurant_representer.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'roar/decorator'
+require 'roar/json'
+require_relative 'restaurant_details_representer'
+
+module Ewa
+  module Representer
+    # Represents searched restaurants infos
+    class SearchedRestaurants < Roar::Decorator
+      include Roar::JSON
+
+      # returns searched rests details by restaurant name 
+      collection :searched_rests, extend: Representer::RestaurantDetails
+    end
+  end
+end
