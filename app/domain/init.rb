15fb67dd00f1e572ad2a990c7644d42af36d483e
rainyjonne <rainyjonne@gmail.com>
2020-12-12 23:28:50 +0800

    Add new repo data ewa-app

--- app/domain/init.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# folders = %w[restaurant_options restaurants]
-folders = %w[restaurants restaurant_options]
-
-folders.each do |folder|
-  require_relative "#{folder}/init.rb"
-end
